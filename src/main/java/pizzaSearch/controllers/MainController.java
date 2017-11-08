8a0c7213a9668f28fdca2846c32f9718fb5a7817
Mateusz Golik <mateusz.golik86@gmail.com>
2017-11-08 17:22:08 +0100

    maven

--- src/main/java/pizzaSearch/controllers/MainController.java
+++ src/main/java/pizzaSearch/controllers/MainController.java
@@ -1,4 +1,20 @@
 package pizzaSearch.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
 public class MainController {
+
+    @FXML
+    Label labelPizza;
+
+    @FXML
+    TextField textCity;
+
+    @FXML
+    Button buttonShow;
+
 }
