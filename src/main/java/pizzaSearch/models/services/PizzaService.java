7d35ac951c4164b18544ff461833147c3fdf42b3
Andrzej Ekiert <andrzej.ekiertt@gmail.com>
2017-11-08 17:33:32 +0100

    maven

--- src/main/java/pizzaSearch/models/services/PizzaService.java
+++ src/main/java/pizzaSearch/models/services/PizzaService.java
@@ -1,17 +1,24 @@
 package pizzaSearch.models.services;
 
-import java.util.ArrayList;
-import java.util.List;
+import pizzaSearch.models.utils.Confing;
+import pizzaSearch.models.utils.HttpUtils;
 
 public class PizzaService {
-
     private static PizzaService ourInstance = new PizzaService();
 
-    public static PizzaService getService() {
+    public static PizzaService getInstance() {
         return ourInstance;
     }
 
+    private PizzaService() {
+    }
 
+    public void makeCall(String city){
+        parseJsonData(HttpUtils.makeHttpRequest(Confing.URL_ID + city + "&key=" + Confing.API_ID));
+    }
+private void parseJsonData(String text){
+
+}
 
 
 }
