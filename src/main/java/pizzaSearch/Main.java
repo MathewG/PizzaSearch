8aca523dfe07724b6133dc03d49bfc064a0ece19
Mateusz Golik <mateusz.golik86@gmail.com>
2017-11-09 13:49:38 +0100

    view

--- src/main/java/pizzaSearch/Main.java
+++ src/main/java/pizzaSearch/Main.java
@@ -7,13 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import pizzaSearch.models.services.PizzaService;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("pizzaView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
